c130408994642f0113e6db8053aac540a8583e85
Ryan terMeulen <Ryan_Termeulen@bose.com>
2021-05-08 22:53:33 -0400

    Added Mouse Layer

--- lets_split/keymaps/ryant/keymap.c
+++ lets_split/keymaps/ryant/keymap.c
@@ -8,7 +8,8 @@
 #define _COLEMAK 1
 #define _LOWER 2
 #define _RAISE 3
-#define _MOVE 4
+#define _MOVE  4
+#define _MOUSE 5
 
 #define _ADJUST 16
 
@@ -62,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* LOWER
  * ,-----------------------------------------------------------------------------------.
- * | ESC  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |      |      |   [  |   ]  |   =  |
+ * | ESC  |      |      |      |      |      |      |      |      |   [  |   ]  |   =  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |   \  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -72,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12( \
-  KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, KC_LBRC, KC_RBRC, KC_EQL, \
+  KC_ESC,  _______, _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_EQL, \
   KC_GRV,  KC_1, 	KC_2,    KC_3,    KC_4,     KC_5,    KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
@@ -81,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* RAISE
  * ,-----------------------------------------------------------------------------------.
- * | ESC  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |   {  |   }  |   +  |
+ * | ESC  |      |      |      |      |      |      |      |      |   {  |   }  |   +  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |   |  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -92,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
  
 [_RAISE] = LAYOUT_ortho_4x12( \
-  KC_PLUS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_LCBR, KC_RCBR, KC_PLUS, \
+  KC_PLUS, _______, _______, _______, _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_PLUS, \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
@@ -100,19 +101,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* MOVE
  * ,-----------------------------------------------------------------------------------.
- * | ESC  |      |      | WWW- | WWW+ |      | MB_C | MB_L | MWH_D| MWH_U| MB_R |      |
+ * | ESC  |  F1  |  F2  |  F3  |  F4  |  F5  | MW_U |      |  Up  |      |      | PGUP |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  | END  | HOME | PGDN | PGUP |      |      | M_L  | M_D  | M_U  | M_R  |      |
+ * | Del  |  F6  |  F7  |  F8  |  F9  |  F10 | MW_D | Left | Down | Right|      | PGDN |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      | UNDO |  CUT | COPY |PASTE |      |      | Left | Down |  Up  |Right |      |
+ * |      |  F11 |  F12 | COPY |PASTE |      | WWW- | WWW+ |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_MOVE] = LAYOUT_ortho_4x12( \
-  KC_ESC,  _______, _______, KC_WBAK, KC_WFWD, _______, KC_BTN3, KC_BTN1, KC_WH_D, KC_WH_U, KC_BTN2, _______, \
-  KC_DEL,  KC_END,  KC_HOME, KC_PGDN, KC_PGUP, _______, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, \
-  _______, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, \
+  KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_WH_U, _______, KC_UP,   _______, _______, KC_PGUP, \
+  KC_DEL,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_WH_D, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_PGDN, \
+  _______, KC_F11,  KC_F12,  KC_COPY, KC_PSTE, _______, KC_WBAK, KC_WFWD, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+),
+
+/* MOUSE
+ * ,-----------------------------------------------------------------------------------.
+ * | ESC  |  F1  |  F2  |  F3  |  F4  |  F5  | MW_U | MB_L | M_U  | MB_R | MB_C | PGUP |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Del  |  F6  |  F7  |  F8  |  F9  |  F10 | MW_D | M_L  | M_D  | M_R  |      | PGDN |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |  F11 |  F12 | COPY |PASTE |      | WWW- | WWW+ |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_MOUSE] = LAYOUT_ortho_4x12( \
+  KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN3, KC_PGUP, \
+  KC_DEL,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, _______, KC_PGDN, \
+  _______, KC_F11,  KC_F12,  KC_COPY, KC_PSTE, _______, KC_WBAK, KC_WFWD, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
@@ -120,7 +139,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|      |      |
+ * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -138,7 +157,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+  state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+  state = update_tri_layer_state(state, _LOWER, _MOVE, _MOUSE);
+  return state;
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {

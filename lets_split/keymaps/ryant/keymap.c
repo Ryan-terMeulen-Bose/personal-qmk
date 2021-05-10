1332d2f66aa86995274a2697fd5eb19350f33e26
Ryan terMeulen <Ryan_Termeulen@bose.com>
2021-05-09 21:06:32 -0400

    fixed prior mistake with tilde

--- lets_split/keymaps/ryant/keymap.c
+++ lets_split/keymaps/ryant/keymap.c
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
  
 [_RAISE] = LAYOUT_ortho_4x12( \
-  KC_ESC,  _______, _______, _______, _______, KC_TILD, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______, \
+  KC_ESC,  _______, _______, _______, _______, _______, _______, _______, KC_TILD, KC_LCBR, KC_RCBR, KC_PLUS, \
   KC_DEL,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
